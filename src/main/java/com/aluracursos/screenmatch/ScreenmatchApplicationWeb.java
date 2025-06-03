62bf2984cbe031db497edd6fdfeeb80b64d99d02
Rene Avila <rene.avila.alonso@gmail.com>
2025-06-02 21:59:23 -0500

    comenzando mapeo y utilizar mvc

---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationWeb.java
@@ -0,0 +1,13 @@
+package com.aluracursos.screenmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplicationWeb{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplicationWeb.class, args);
+	}
+
+}
